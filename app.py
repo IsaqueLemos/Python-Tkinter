e6059b399181a83bb44a2e188b79226af7511e67
Isaque Lemos <88510197+IsaqueLemos@users.noreply.github.com>
2022-03-29 19:50:43 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,52 +1,56 @@
 from tkinter import *
 from tkinter.ttk import Combobox
 
+# Criando arquivo para salvar dados
+arquivo = open('dados.txt', 'a')
+arquivo.close()
+
+def validar(legenda, condicao):
+    if condicao:
+        legenda.configure(fg="limegreen")
+    else:
+        legenda.configure(fg='red')
+
+def validar_nascimento(legenda, condicao, data):
+    if condicao and int(data[:2]) < 32 and int(data[2:4]) < 13 and int(data[4:8]) < 2023:
+        legenda.configure(fg='limegreen')
+    else:
+        legenda.configure(fg='red')
+
 def salvar():
     arquivo = open('dados.txt', 'a')
     dados = []
-    
+
+    ### Recolhendo dados
     nome = en_nome.get()
-    dados.append(nome)
-    c1 = all(char.isalpha() or char.isspace() for char in nome)
-    if c1:
-        lb_nome.configure(fg='limegreen')
-    else:
-        lb_nome.configure(fg='red')
-
     cpf = en_cpf.get()
-    dados.append(f'{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:11]}')
-    c2 = len(cpf) == 11 and cpf.isdigit()
-    if c2:
-        lb_cpf.configure(fg='limegreen')
-    else:
-        lb_cpf.configure(fg='red')
-
     matricula = en_matricula.get()
-    dados.append(matricula)
-    c3 = len(matricula) < 20 and matricula.isdigit()
-    if c3:
-        lb_matricula.configure(fg='limegreen')
-    else:
-        lb_matricula.configure(fg='red')
-
     data = en_data.get()
-    dados.append(f'{data[:2]}/{data[2:4]}/{data[4:8]}')
-    dia = int(data[:2])
-    mes = int(data[2:4])
-    ano = int(data[4:8])
-    c4 = len(data) == 8 and dia < 32 and mes < 13 and ano < 2023
-    if c4:
-        lb_data.configure(fg='limegreen')
-    else:
-        lb_data.configure(fg='red')
 
-    if c1 and c2 and c3 and c4:
+    ### Criando condicionais
+    c_nome = all(char.isalpha() or char.isspace() for char in nome)
+    c_cpf = len(cpf) == 11 and cpf.isdigit()
+    c_matricula = len(matricula) < 20 and matricula.isdigit()
+    c_data = len(data) == 8 and data.isdigit()
+    
+    ### Validação de dados inseridos
+    validar(lb_nome, c_nome)
+    validar(lb_cpf, c_cpf)
+    validar(lb_matricula, c_matricula)
+    validar_nascimento(lb_data, c_data, data)
+
+    ### Adicionando arquivos num arquivo de texto
+    if c_nome and c_cpf and c_matricula and c_data:
+        dados.append(nome)
+        dados.append(f'{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:11]}')
+        dados.append(matricula)
+        dados.append(f'{data[:2]}/{data[2:4]}/{data[4:8]}')
+        
         arquivo.write('%'.join(dados))
         arquivo.write('\n')
         arquivo.close()
         janela.destroy()
     else:
-        dados = []
         janela.bell()
 
 def verificar():
@@ -64,7 +68,7 @@ def dados_salvos():
     janela2.title('Dados Salvos')
     janela2.geometry('500x400')
     janela2.resizable(False, False)
-    janela2['bg'] = '#121212'
+    janela2['bg'] = '#1D165C'
 
     ### Lendo e agrupando dados 
     arquivo = open('dados.txt', 'r')
@@ -78,23 +82,23 @@ def dados_salvos():
     arquivo.close()
 
     ### Lista de Dados na Interface
-    Label(janela2, text='Dados:', font='arial 14', bg='#121212', fg='white').place(x=25, y=30)
+    Label(janela2, text='Dados:', font='arial 14', bg='#1D165C', fg='white').place(x=25, y=30)
     tb = Combobox(janela2, value=nomes, font='arial 14')
     tb.place(x=25, y=60)
 
     ### Botao Verificar
-    Button(janela2, text='Verificar', bg='#00026B', fg='white', font='arial 18', command=verificar).place(x=350, y=45)
+    Button(janela2, text='Verificar', bg='#E0A819', fg='whitesmoke', font='arial 18', command=verificar).place(x=350, y=45)
 
     ### CPF
-    dt_cpf = Label(janela2, text='CPF:', fg='White', font='arial 17', bg='#121212')
+    dt_cpf = Label(janela2, text='CPF:', fg='White', font='arial 17', bg='#1D165C')
     dt_cpf.place(x=25, y=190)
 
     ### Matrícula
-    dt_matricula = Label(janela2, text='Matrícula:', fg='White', font='arial 17', bg='#121212')
+    dt_matricula = Label(janela2, text='Matrícula:', fg='White', font='arial 17', bg='#1D165C')
     dt_matricula.place(x=25, y=220)
 
     ### Data de Nascimento
-    dt_data = Label(janela2, text='Data de Nascimento:', fg='White', font='arial 17', bg='#121212')
+    dt_data = Label(janela2, text='Data de Nascimento:', fg='White', font='arial 17', bg='#1D165C')
     dt_data.place(x=25, y=250)
 
     ### Manter na tela
@@ -109,34 +113,34 @@ def cadastrar():
     janela.title('Cadastro')
     janela.geometry('500x400')
     janela.resizable(False, False)
-    janela['bg'] = '#121212'
+    janela['bg'] = '#1D165C'
 
     ### Nome 
-    lb_nome = Label(janela, text = 'Nome:', bg='#121212', fg='white', font='arial 14')
+    lb_nome = Label(janela, text = 'Nome:', bg='#1D165C', fg='white', font='arial 14')
     lb_nome.place(x= 85, y=20)
     en_nome = Entry(janela, font='arial 14', width=30)
     en_nome.place(x= 85, y=50)
 
     ### CPF
-    lb_cpf = Label(janela, text = 'CPF:', bg='#121212', fg='white', font='arial 14')
+    lb_cpf = Label(janela, text = 'CPF:', bg='#1D165C', fg='white', font='arial 14')
     lb_cpf.place(x= 85, y=100)
     en_cpf = Entry(janela, font='arial 14', width=30)
     en_cpf.place(x= 85, y=130)
 
     ### Matricula
-    lb_matricula = Label(janela, text = 'Número de Matrícula:', bg='#121212', fg='white', font='arial 14')
+    lb_matricula = Label(janela, text = 'Número de Matrícula:', bg='#1D165C', fg='white', font='arial 14')
     lb_matricula.place(x= 85, y=180)
     en_matricula = Entry(janela, font='arial 14', width=30)
     en_matricula.place(x= 85, y=210)
 
     ### Data
-    lb_data = Label(janela, text = 'Data de Nascimento:', bg='#121212', fg='white', font='arial 14')
+    lb_data = Label(janela, text = 'Data de Nascimento:', bg='#1D165C', fg='white', font='arial 14')
     lb_data.place(x= 85, y=260)
     en_data = Entry(janela, font='arial 14', width=30)
     en_data.place(x= 85, y=290)
 
     ### Botao
-    Button(janela, text='Cadastrar', bg='#00026B', fg='white', font='arial 14', command=salvar).place(x= 325, y=340)
+    Button(janela, text='Cadastrar', bg='#E0A819', fg='whitesmoke', font='arial 14', command=salvar).place(x= 325, y=340)
 
     ### Manter na tela
     janela.mainloop()
@@ -146,9 +150,11 @@ tela = Tk()
 tela.title('Formulário')
 tela.geometry('500x200')
 tela.resizable(False, False)
-tela['bg'] = '#121212'
+tela['bg'] = '#1D165C'
 
-Button(text='Formulário', font='arial 18', fg='white', bg='#F2CB07', command=cadastrar).place(x=80, y=70)
-Button(text='Dados Salvos', font='arial 18', fg='white', bg='#F2CB07', command=dados_salvos).place(x=250, y=70)
+Label(text='Sistema de Cadastro', font='arial 20', bg='#1D165C', fg='white').place(x=120, y=20)
+Button(text='Cadastrar', font='arial 18', fg='whitesmoke', bg='#E0A819', command=cadastrar).place(x=100, y=80)
+Button(text='Dados Salvos', font='arial 18', fg='whitesmoke', bg='#E0A819', command=dados_salvos).place(x=250, y=80)
+Label(text='CÓDIGO FONTE: ISAQUE', font='arial 10', bg='#1D165C', fg='#2B6CE6').place(x=320, y=170)
 
 tela.mainloop()
